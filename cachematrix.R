d03bc90eb24560ef9ff71a62869455e419afb276
jnremachaGH <jnremachaGH@gmail.com>
2020-07-27 17:00:23 +0200

    Correcting comments

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,7 @@
 
 ## Write a short comment describing this function
 
-## The first function, makeVector creates a special "vector", 
+## The first function, makeCacheMatrix creates a special "vector", 
 ## which is really a list containing a function to:
 
 ## set the value of the matrix
@@ -33,10 +33,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-## The following function calculates the mean of the special "vector" created 
-## with the above function. However, it first checks to see if the mean has
+## The following function calculates the inverse of the special "vector" created 
+## with the above function. However, it first checks to see if the inverse has
 ## already been calculated. If so, it gets the inverse from the cache and 
-## skips the computation. Otherwise, it calculates the inverse of the data
+## skips the computation. Otherwise, it calculates the inverse 
 ## and sets the value of the inverse in the cache via the setinverse function.
 
 
